2fabbeed4733c83f4bf315208a637fcb158bd77c
Lee9747 <leejunho32@naver.com>
2023-12-08 20:40:11 +0900

    전투기 obj 추가

--- STEALTH/config.h
+++ STEALTH/config.h
@@ -10,6 +10,9 @@
 #include <gl/glm/glm.hpp> //  이 include들을 다른데로 옮기지 말 것!
 #include <gl/glm/ext.hpp> 
 #include <gl/glm/gtc/matrix_transform.hpp>
+//#include <gl/glm/glm/glm.hpp>
+//#include <gl/glm/glm/ext.hpp>
+//#include <gl/glm/glm/gtc/matrix_transform.hpp>
 #include <gl/glew.h>
 #include <gl/freeglut.h>
 #include <gl/freeglut_ext.h>
